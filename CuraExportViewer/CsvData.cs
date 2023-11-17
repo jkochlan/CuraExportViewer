5081b9d5ff85dc0f713b19abaaac2f1f093296e0
jkochlan <jan.kochlan@gmail.com>
2023-11-17 05:13:12 +0100

    init

---
+++ CuraExportViewer/CsvData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuraExportViewer
+{
+    public class CsvData
+    {
+        public string Section { get; set; }
+        public string Extruder { get; set; }
+        public string Key { get; set; }
+        public string Type { get; set; }
+        public string Value { get; set; }
+    }
+}
---
+++ CuraExportViewer/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using System.IO;
+using Path = System.IO.Path;
+
+namespace CuraExportViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> CsvFiles { get; set; }
+        public ObservableCollection<CsvData> CsvDataList { get; set; }
+
+        private string folderPath = @"D:\Private\3Dprint";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CsvFiles = new ObservableCollection<string>();
+            CsvDataList = new ObservableCollection<CsvData>();
+
+            // Set the ListBox item source
+            csvListBox.ItemsSource = CsvFiles;
+
+            // Set the DataGrid item source
+            csvDataGrid.ItemsSource = CsvDataList;
+
+            // Specify the folder path where your CSV files are located
+            
+
+            // Populate the ListBox with CSV files
+            foreach (string filePath in Directory.EnumerateFiles(folderPath, "*.csv"))
+            {
+                CsvFiles.Add(Path.GetFileName(filePath));
+            }
+
+            // Handle selection changed event
+            csvListBox.SelectionChanged += CsvListBox_SelectionChanged;
+        }
+
+        private void CsvListBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            // Clear previous data
+            CsvDataList.Clear();
+
+            // Get the selected CSV file
+            string selectedCsvFile = csvListBox.SelectedItem as string;
+
+            if (selectedCsvFile != null)
+            {
+                // Read and parse the CSV file
+                string filePath = Path.Combine(folderPath, selectedCsvFile);
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines.Skip(1)) // Skip header
+                {
+                    string[] values = line.Split(';');
+                    CsvDataList.Add(new CsvData
+                    {
+                        Section = values[0],
+                        Extruder = values[1],
+                        Key = values[2],
+                        Type = values[3],
+                        Value = values[4]
+                    });
+                }
+            }
+        }
+    }
+
+}
